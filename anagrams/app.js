9682670efe829a2be48a11623f5b54df29e8987e
rifaq-ahmer <rifaq.ahmer@gmail.com>
2020-11-29 15:37:07 +0530

    final

--- anagrams/app.js
+++ anagrams/app.js
@@ -1,26 +1,36 @@
 function anagrams(stringA, stringB) {
-	const aCharMap = buildCharMap(stringA);
-	const bCharMap = buildCharMap(stringB);
-
-	if (Object.keys(aCharMap).length !== Object.keys(bCharMap).length) {
-		return false;
-	}
-
-	for (let char in aCharMap) {
-		if (aCharMap[char] !== bCharMap[char]) {
-			return false;
-		}
-	}
-	return true;
+	return cleanStrings(stringA) === cleanStrings(stringB);
 }
 
-function buildCharMap(str) {
-	const charMap = {};
-	for (const char in str.replace(/[^\w]/g, "").toLowerCase()) {
-		charMap[char] = charMap[char] + 1 || 1;
-	}
-	return charMap;
+function cleanStrings(str) {
+	return str.replace((/[^\w]/g, "").toLowerCase().split("").sort().join(""));
 }
+
+//---------Possible Solition------
+
+// function anagrams(stringA, stringB) {
+// 	const aCharMap = buildCharMap(stringA);
+// 	const bCharMap = buildCharMap(stringB);
+
+// 	if (Object.keys(aCharMap).length !== Object.keys(bCharMap).length) {
+// 		return false;
+// 	}
+
+// 	for (let char in aCharMap) {
+// 		if (aCharMap[char] !== bCharMap[char]) {
+// 			return false;
+// 		}
+// 	}
+// 	return true;
+// }
+
+// function buildCharMap(str) {
+// 	const charMap = {};
+// 	for (const char in str.replace(/[^\w]/g, "").toLowerCase()) {
+// 		charMap[char] = charMap[char] + 1 || 1;
+// 	}
+// 	return charMap;
+// }
 
 console.log(anagrams("RAIL ! SAFETY", "FAIry Tales"));
 console.log(anagrams("Hi there", "Bye There"));
